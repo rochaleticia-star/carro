2c3d04df012837815b557386e7322e8c0096ec2d
rochaleticia-star <rochaleticia3267@gmail.com>
2024-08-12 17:18:42 -0300

    colocando arquivo p5js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+    background("lightblue");
+  
+  }
+  
+  function draw() {
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,30,50);
+    
+  }
+    
+    
+  stroke("black")
+  fill("blue")
+  
+  }
